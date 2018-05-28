267643c49e7319acb9a73fe063e1170d619c8ae2
James Wang <shijun.james@gmail.com>
2018-05-28 16:09:33 +0800

    adding ArrayList into the solution

--- ClubManager/src/Workshop/Club.java
+++ ClubManager/src/Workshop/Club.java
@@ -1,10 +1,13 @@
 package Workshop;
 
+import java.util.ArrayList; // need to import this for ArrayList
+
 public class Club {
 	
 	private static int currentNumber = 0001;
 	private static int counter = 0;
 	private static Member[] memberList = new Member[1];
+	private static ArrayList<Member> mList= new ArrayList<Member>();
 	
 	public Member addMember(String surName, String firstName, String secondName) {
 		Member a = new Member(surName, firstName, secondName, currentNumber);
@@ -14,28 +17,34 @@ public class Club {
 		
 	public void addMember(String surName, String firstName) {
 		Member a = new Member(surName, firstName,currentNumber);
-		addToList(a);
+		addToList(a);		
 		}
 	
 	private void addToList(Member a) {
-		memberList[counter]=a;		
+//		memberList[counter]=a;
+		mList.add(counter,a);
 		counter ++;
 		currentNumber ++;
-		Member[] newList = new Member[(counter+1)];
-		System.arraycopy(memberList, 0, newList, 0, memberList.length);
-		memberList = newList;
+//		Member[] newList = new Member[(counter+1)];
+//		System.arraycopy(memberList, 0, newList, 0, memberList.length);
+//		memberList = newList;
 	}		
 	
 	public void showMember() {
-		for (int i = 0; i < memberList.length; i++) {
-			if (memberList[i]!=null) {			
-			System.out.println(memberList[i]);
-			}
-		}	
+		for (Member m : mList) {			
+			System.out.println(m);			
+		}
+		
+//		for (int i = 0; i < memberList.length; i++) {
+//			if (memberList[i]!=null) {			
+//			System.out.println(memberList[i]);
+//			}
+//		}	
 	}
 	
 	public void removeMember(int a) {
-		memberList[a-1]=null;		
+		mList.remove(a-1);
+//		memberList[a-1]=null;		
 	}
 }
 
--- ClubManager/src/Workshop/ClubApplication.java
+++ ClubManager/src/Workshop/ClubApplication.java
@@ -4,31 +4,6 @@ public class ClubApplication {
 
 	public static void main(String[] args) {
 		
-//		System.out.println("Hello World");
-//		
-//		Person a = new Person("Wang","Shijun","James");
-//		Person b = new Person("Wang","Shijun");
-//		
-//		a.show();
-//		b.show();
-//		
-//		Facility c = new Facility("Ballroom", "Can hold up to 45 tables");
-//		Facility d = new Facility("Pool");
-//		
-//		c.show();
-//		d.show();
-//		
-//		Member aa = new Member("Wang","Shijun","James", 1000);
-//		Member bb = new Member("Wang","Shijun",1001);
-//		Person cc = new Member("Wang","Shijun","James", 1002);
-//		Person dd = new Member("Wang","Shijun",1003);
-//		
-//		
-//		aa.show();
-//		bb.show();
-//		cc.show();
-//		dd.show();
-		
 		Club aaa = new Club();
 		Club bbb = new Club();
 		aaa.addMember("Wang","Shijun","James").show();
